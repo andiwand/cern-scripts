9fd4c3e5d26f0f49eee47308503a15da7a92fde6
Andreas Stefl <stefl.andreas@gmail.com>
2024-04-03 09:33:33 +0200

    generalize acts setup script

--- acts/setup-dev.sh
+++ acts/setup-dev.sh
@@ -13,40 +13,46 @@ name=$1
 source=~/cern/source/acts/acts/$name
 build=~/cern/build/acts/acts/$name
 
-# check if directory exists
-if [[ -d "$source" || -d "$build" ]]; then
-    echo "directory $source already exists"
-    exit 1
+if [[ ! -d "$source" ]]; then
+    echo "create source directory: $source"
+
+    git clone git@github.com:acts-project/acts.git $source
+    cd $source
+    git remote rm origin
+    git remote add origin git@github.com:andiwand/acts.git
+    git remote add upstream git@github.com:acts-project/acts.git
+    git fetch --all
+    git submodule update --init --recursive
+
+    git maintenance start
+else
+    echo "source directory already exists: $source"
 fi
-
-git clone git@github.com:acts-project/acts.git $source
-cd $source
-git remote rm origin
-git remote add origin git@github.com:andiwand/acts.git
-git remote add upstream git@github.com:acts-project/acts.git
-git fetch --all
-git submodule update --init --recursive
-
-git maintenance start
 
 # TODO the activate script needs to be configured
 cp "$script_dir/activate.sh" "$source"
 
 source "$source/activate.sh"
 
-cmake -S "$source" -B "$build" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_C_COMPILER=clang \
-  -DCMAKE_CXX_COMPILER=clang++ \
-  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DACTS_FORCE_ASSERTIONS=ON \
-  -DACTS_BUILD_EXAMPLES_DD4HEP=ON \
-  -DACTS_BUILD_EXAMPLES_GEANT4=ON \
-  -DACTS_BUILD_EXAMPLES_PYTHON_BINDINGS=ON \
-  -DACTS_BUILD_FATRAS=ON \
-  -DACTS_BUILD_FATRAS_GEANT4=ON \
-  -DACTS_BUILD_ODD=ON \
-  -DACTS_BUILD_UNITTESTS=ON \
-  -DACTS_BUILD_INTEGRATIONTESTS=ON
+if [[ -d "$build" ]]; then
+    echo "create build directory: $build"
+
+    cmake -S "$source" -B "$build" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_C_COMPILER=clang \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DACTS_FORCE_ASSERTIONS=ON \
+        -DACTS_BUILD_EXAMPLES_DD4HEP=ON \
+        -DACTS_BUILD_EXAMPLES_GEANT4=ON \
+        -DACTS_BUILD_EXAMPLES_PYTHON_BINDINGS=ON \
+        -DACTS_BUILD_FATRAS=ON \
+        -DACTS_BUILD_FATRAS_GEANT4=ON \
+        -DACTS_BUILD_ODD=ON \
+        -DACTS_BUILD_UNITTESTS=ON \
+        -DACTS_BUILD_INTEGRATIONTESTS=ON
+else
+    echo "build directory already exists: $build"
+fi
