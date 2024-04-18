ef1dca16bbe9fbe56516f3c5fcd693806dfae25c
Andreas Stefl <stefl.andreas@gmail.com>
2024-04-18 10:37:19 +0200

    improve build all script

--- acts/deps/setup.sh
+++ acts/deps/setup.sh
@@ -37,6 +37,8 @@ source_tree=~/cern/tmp/source
 build_tree=
 install_tree=
 
+activate_script=
+
 ###
 # OS detection
 ###
@@ -64,24 +66,12 @@ if [[ -z "$os_id" ]]; then
 fi
 
 ###
-# Dependencies of the dependencies
-###
-
-if [[ "$os_id" == "ubuntu" ]]; then
-    sudo apt-get install -y \
-        build-essential \
-        cmake \
-        libboost-all-dev \
-        ninja-build
-elif [[ "$os_id" == "macos" ]]; then
-    brew install \
-        boost \
-        xerces-c
-fi
-
-###
 # Download and unpack
 ###
+
+echo "###"
+echo "# Download and unpack"
+echo "###"
 
 download_unpack() {
     local source=$1
@@ -103,147 +93,232 @@ download_unpack() {
     rm "${destination}_download"
 }
 
+echo "json version: ${json_version}"
 download_unpack "$json_url" "${source_tree}/json/${json_version}"
+
+echo "tbb version: ${tbb_version}"
 download_unpack "$tbb_url" "${source_tree}/tbb/${tbb_version}"
+
+echo "eigen version: ${eigen_version}"
 download_unpack "$eigen_url" "${source_tree}/eigen/${eigen_version}"
+
+echo "root version: ${root_version}"
 download_unpack "$root_url" "${source_tree}/root/${root_version}"
+
+echo "geant4 version: ${geant4_version}"
 download_unpack "$geant4_url" "${source_tree}/geant4/${geant4_version}"
+
+echo "pythia8 version: ${pythia8_version}"
 download_unpack "$pythia8_url" "${source_tree}/pythia8/${pythia8_version}"
+
+echo "podio version: ${podio_version}"
 download_unpack "$podio_url" "${source_tree}/podio/${podio_version}"
+
+echo "edm4hep version: ${edm4hep_version}"
 download_unpack "$edm4hep_url" "${source_tree}/edm4hep/${edm4hep_version}"
+
+echo "dd4hep version: ${dd4hep_version}"
 download_unpack "$dd4hep_url" "${source_tree}/dd4hep/${dd4hep_version}"
+
+echo "hepmc3 version: ${hepmc3_version}"
 download_unpack "$hepmc3_url" "${source_tree}/hepmc3/${hepmc3_version}"
 
 ###
 # Configure, build and install
 ###
 
-# json
-cmake -S "${source_tree}/json/${json_version}" -B "${build_tree}/json/${json_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/json/${json_version}" \
-  -DJSON_BuildTests=OFF
-cmake --build "${build_tree}/json/${json_version}" --target install
+function build_json() {
+    cmake -S "${source_tree}/json/${json_version}" -B "${build_tree}/json/${json_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/json/${json_version}" \
+        -DJSON_BuildTests=OFF
+    cmake --build "${build_tree}/json/${json_version}" --target install
+}
 
-# tbb
-cmake -S "${source_tree}/tbb/${tbb_version}" -B "${build_tree}/tbb/${tbb_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/tbb/${tbb_version}" \
-  -DTBB_TEST=OFF
-cmake --build "${build_tree}/tbb/${tbb_version}" --target install
+function build_tbb() {
+    cmake -S "${source_tree}/tbb/${tbb_version}" -B "${build_tree}/tbb/${tbb_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/tbb/${tbb_version}" \
+        -DTBB_TEST=OFF
+    cmake --build "${build_tree}/tbb/${tbb_version}" --target install
+}
 
-# eigen
-cmake -S "${source_tree}/eigen/${eigen_version}" -B "${build_tree}/eigen/${eigen_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/eigen/${eigen_version}"
-cmake --build "${build_tree}/eigen/${eigen_version}" --target install
+function build_eigen() {
+    cmake -S "${source_tree}/eigen/${eigen_version}" -B "${build_tree}/eigen/${eigen_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/eigen/${eigen_version}"
+    cmake --build "${build_tree}/eigen/${eigen_version}" --target install
+}
 
-# root
-cmake -S "${source_tree}/root/${root_version}" -B "${build_tree}/root/${root_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/root/${root_version}" \
-  -Dfail-on-missing=ON \
-  -Dgdml=ON \
-  -Dx11=ON \
-  -Dpyroot=On \
-  -Ddataframe=ON \
-  -Dmysql=OFF \
-  -Doracle=OFF \
-  -Dpgsql=OFF \
-  -Dsqlite=OFF \
-  -Dpythia6=OFF \
-  -Dpythia8=OFF \
-  -Dfftw3=OFF \
-  -Dbuiltin_cfitsio=ON \
-  -Dbuiltin_xxhash=ON \
-  -Dbuiltin_afterimage=ON \
-  -Dbuiltin_openssl=ON \
-  -Dbuiltin_ftgl=ON \
-  -Dbuiltin_glew=ON \
-  -Dbuiltin_gsl=ON \
-  -Dbuiltin_gl2ps=ON \
-  -Dbuiltin_xrootd=ON \
-  -Dbuiltin_pcre=ON \
-  -Dbuiltin_lzma=ON \
-  -Dbuiltin_zstd=ON \
-  -Dbuiltin_lz4=ON \
-  -Dgfal=OFF \
-  -Ddavix=OFF \
-  -Dbuiltin_vdt=ON \
-  -Dxrootd=OFF \
-  -Dtmva=OFF
-cmake --build "${build_tree}/root/${root_version}" --target install
+function build_root() {
+    export CMAKE_PREFIX_PATH="${install_tree}/json/${json_version}"
+    cmake -S "${source_tree}/root/${root_version}" -B "${build_tree}/root/${root_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/root/${root_version}" \
+        -Dfail-on-missing=ON \
+        -Dgdml=ON \
+        -Dx11=ON \
+        -Dpyroot=On \
+        -Ddataframe=ON \
+        -Dmysql=OFF \
+        -Doracle=OFF \
+        -Dpgsql=OFF \
+        -Dsqlite=OFF \
+        -Dpythia6=OFF \
+        -Dpythia8=OFF \
+        -Dfftw3=OFF \
+        -Dbuiltin_cfitsio=ON \
+        -Dbuiltin_xxhash=ON \
+        -Dbuiltin_afterimage=ON \
+        -Dbuiltin_openssl=ON \
+        -Dbuiltin_ftgl=ON \
+        -Dbuiltin_glew=ON \
+        -Dbuiltin_gsl=ON \
+        -Dbuiltin_gl2ps=ON \
+        -Dbuiltin_xrootd=ON \
+        -Dbuiltin_pcre=ON \
+        -Dbuiltin_lzma=ON \
+        -Dbuiltin_zstd=ON \
+        -Dbuiltin_lz4=ON \
+        -Dgfal=OFF \
+        -Ddavix=OFF \
+        -Dbuiltin_vdt=ON \
+        -Dxrootd=OFF \
+        -Dtmva=OFF
+    cmake --build "${build_tree}/root/${root_version}" --target install
+}
 
-# geant4
-cmake -S "${source_tree}/geant4/${geant4_version}" -B "${build_tree}/geant4/${geant4_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/geant4/${geant4_version}" \
-  -DGEANT4_BUILD_TLS_MODEL=global-dynamic \
-  -DGEANT4_INSTALL_DATA=ON \
-  -DGEANT4_USE_GDML=ON \
-  -DGEANT4_USE_SYSTEM_EXPAT=ON \
-  -DGEANT4_USE_SYSTEM_ZLIB=ON
-cmake --build "${build_tree}/geant4/${geant4_version}" --target install
+function build_geant4() {
+    cmake -S "${source_tree}/geant4/${geant4_version}" -B "${build_tree}/geant4/${geant4_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/geant4/${geant4_version}" \
+        -DGEANT4_BUILD_TLS_MODEL=global-dynamic \
+        -DGEANT4_INSTALL_DATA=ON \
+        -DGEANT4_USE_GDML=ON \
+        -DGEANT4_USE_SYSTEM_EXPAT=ON \
+        -DGEANT4_USE_SYSTEM_ZLIB=ON
+    cmake --build "${build_tree}/geant4/${geant4_version}" --target install
+}
 
-# pythia8
-cd "${source_tree}/pythia8/${pythia8_version}"
-./configure --enable-shared --prefix="${install_tree}/pythia8/${pythia8_version}"
-make -j12 install
+function build_pythia() {
+    cd "${source_tree}/pythia8/${pythia8_version}"
+    ./configure --enable-shared --prefix="${install_tree}/pythia8/${pythia8_version}"
+    make -j12 install
+}
 
-# podio
-export CMAKE_PREFIX_PATH="${install_tree}/root/${root_version}"
-cmake -S "${source_tree}/podio/${podio_version}" -B "${build_tree}/podio/${podio_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/podio/${podio_version}" \
-  -DBUILD_TESTING=OFF \
-  -USE_EXTERNAL_CATCH2=OFF
-cmake --build "${build_tree}/podio/${podio_version}" --target install
+function build_podio() {
+    export CMAKE_PREFIX_PATH="${install_tree}/root/${root_version}"
+    cmake -S "${source_tree}/podio/${podio_version}" -B "${build_tree}/podio/${podio_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/podio/${podio_version}" \
+        -DBUILD_TESTING=OFF \
+        -USE_EXTERNAL_CATCH2=OFF
+    cmake --build "${build_tree}/podio/${podio_version}" --target install
+}
 
-# edm4hep
-export CMAKE_PREFIX_PATH="${install_tree}/root/${root_version}:${install_tree}/podio/${podio_version}"
-cmake -S "${source_tree}/edm4hep/${edm4hep_version}" -B "${build_tree}/edm4hep/${edm4hep_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/edm4hep/${edm4hep_version}" \
-  -DBUILD_TESTING=OFF \
-  -DUSE_EXTERNAL_CATCH2=OFF
-cmake --build "${build_tree}/edm4hep/${edm4hep_version}" --target install
+function build_edm4hep() {
+    export CMAKE_PREFIX_PATH="${install_tree}/root/${root_version}:${install_tree}/podio/${podio_version}"
+    cmake -S "${source_tree}/edm4hep/${edm4hep_version}" -B "${build_tree}/edm4hep/${edm4hep_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/edm4hep/${edm4hep_version}" \
+        -DBUILD_TESTING=OFF \
+        -DUSE_EXTERNAL_CATCH2=OFF
+    cmake --build "${build_tree}/edm4hep/${edm4hep_version}" --target install
+}
 
-# dd4hep
-export CMAKE_PREFIX_PATH="${install_tree}/root/${root_version}:${install_tree}/podio/${podio_version}:${install_tree}/edm4hep/${edm4hep_version}"
-cmake -S "${source_tree}/dd4hep/${dd4hep_version}" -B "${build_tree}/dd4hep/${dd4hep_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/dd4hep/${dd4hep_version}" \
-  -DBUILD_TESTING=OFF \
-  -DDD4HEP_BUILD_PACKAGES="DDG4 DDDetectors DDRec UtilityApps" \
-  -DDD4HEP_USE_GEANT4=ON \
-  -DDD4HEP_USE_XERCESC=ON \
-  -DDD4HEP_USE_EDM4HEP=ON
-cmake --build "${build_tree}/dd4hep/${dd4hep_version}" --target install
+function build_dd4hep() {
+    export CMAKE_PREFIX_PATH="${install_tree}/root/${root_version}:${install_tree}/podio/${podio_version}:${install_tree}/edm4hep/${edm4hep_version}"
+    cmake -S "${source_tree}/dd4hep/${dd4hep_version}" -B "${build_tree}/dd4hep/${dd4hep_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/dd4hep/${dd4hep_version}" \
+        -DBUILD_TESTING=OFF \
+        -DDD4HEP_BUILD_PACKAGES="DDG4 DDDetectors DDRec UtilityApps" \
+        -DDD4HEP_USE_GEANT4=ON \
+        -DDD4HEP_USE_XERCESC=ON \
+        -DDD4HEP_USE_EDM4HEP=ON
+    cmake --build "${build_tree}/dd4hep/${dd4hep_version}" --target install
+}
 
-# hepmc3
-cmake -S "${source_tree}/hepmc3/${hepmc3_version}" -B "${build_tree}/hepmc3/${hepmc3_version}" \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_INSTALL_PREFIX="${install_tree}/hepmc3/${hepmc3_version}" \
-  -DHEPMC3_BUILD_STATIC_LIBS=OFF \
-  -DHEPMC3_ENABLE_PYTHON=OFF \
-  -DHEPMC3_ENABLE_ROOTIO=OFF \
-  -DHEPMC3_ENABLE_SEARCH=OFF
-cmake --build "${build_tree}/hepmc3/${hepmc3_version}" --target install
+function build_edm4hep() {
+    export CMAKE_PREFIX_PATH="${install_tree}/root/${root_version}:${install_tree}/podio/${podio_version}"
+    cmake -S "${source_tree}/edm4hep/${edm4hep_version}" -B "${build_tree}/edm4hep/${edm4hep_version}" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_PREFIX="${install_tree}/edm4hep/${edm4hep_version}" \
+        -DBUILD_TESTING=OFF \
+        -DUSE_EXTERNAL_CATCH2=OFF
+    cmake --build "${build_tree}/edm4hep/${edm4hep_version}" --target install
+}
+
+echo "###"
+echo "# Configure, build and install"
+echo "###"
+
+echo "json version: ${json_version}"
+build_json
+
+echo "tbb version: ${tbb_version}"
+build_tbb
+
+echo "eigen version: ${eigen_version}"
+build_eigen
+
+echo "root version: ${root_version}"
+build_root
+
+echo "geant4 version: ${geant4_version}"
+build_geant4
+
+echo "pythia8 version: ${pythia8_version}"
+build_pythia8
+
+echo "podio version: ${podio_version}"
+build_podio
+
+echo "edm4hep version: ${edm4hep_version}"
+build_edm4hep
+
+echo "dd4hep version: ${dd4hep_version}"
+build_dd4hep
+
+echo "hepmc3 version: ${hepmc3_version}"
+build_edm4hep
+
+###
+# Activate script
+###
+
+echo "###"
+echo "# Activate script"
+echo "###"
+
+echo "#!/usr/bin/env bash" > "${activate_script}"
+echo "" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/json/${json_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/tbb/${tbb_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/eigen/${eigen_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/root/${root_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/geant4/${geant4_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/pythia8/${pythia8_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/podio/${podio_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/edm4hep/${edm4hep_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/dd4hep/${dd4hep_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "export CMAKE_PREFIX_PATH=\"${install_tree}/hepmc3/${hepmc3_version}:\${CMAKE_PREFIX_PATH}\"" >> "${activate_script}"
+echo "" >> "${activate_script}"
