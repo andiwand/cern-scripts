585332ddda8ff4f87596d2b388a15862ac168f2c
Andreas Stefl <stefl.andreas@gmail.com>
2025-07-10 13:46:46 +0200

    spack setup

---
+++ acts/setup-dev-spack.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+# check if parameter is given
+if [ -z "$1" ]; then
+    echo "$0 <name>"
+    exit 1
+fi
+
+script=$(readlink -f "$0")
+script_dir=$(dirname "$script")
+
+name=$1
+source=$(pwd)/source/acts/acts/$name
+build=$(pwd)/build/acts/acts/$name
+install_base=$(pwd)/install
+spack_base=$(pwd)/source/spack
+spack_env=$(pwd)/source/acts/ci-dependencies
+python_env=$(pwd)/source/acts/ci-dependencies/.python-env
+
+if [[ ! -d "$source" ]]; then
+    echo "create source directory: $source"
+
+    git clone git@github.com:acts-project/acts.git $source
+    cd $source
+    git remote rm origin
+    git remote add origin git@github.com:andiwand/acts.git
+    git remote add upstream git@github.com:acts-project/acts.git
+    git fetch --all
+    git submodule update --init --recursive
+
+    git maintenance start
+
+    echo "" > "$source/.clangd"
+    echo "CompileFlags:" >> "$source/.clangd"
+    echo "  CompilationDatabase: $build" >> "$source/.clangd"
+
+    echo "" > "$source/activate.sh"
+    echo ". ${spack_base}/share/spack/setup-env.sh" >> "$source/activate.sh"
+    echo "" > "$source/activate.sh"
+    echo "spack env activate ${spack_env}" >> "$source/activate.sh"
+    echo "" > "$source/activate.sh"
+    echo ". ${python_env}/bin/activate" >> "$source/activate.sh"
+    echo "" >> "$source/activate.sh"
+    echo ". $build/this_acts_withdeps.sh" >> "$source/activate.sh"
+else
+    echo "source directory already exists: $source"
+fi
+
+if [[ ! -d "$build" ]]; then
+    echo "create build directory: $build"
+
+    source "${spack_base}/share/spack/setup-env.sh"
+    spack env activate "${spack_env}"
+
+    source "${python_env}/bin/activate"
+
+    cmake -S "$source" -B "$build" \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_C_COMPILER=clang \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+        -DCMAKE_CXX_STANDARD=20 \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DPython_ROOT_DIR="${python_env}" \
+        -DPython_FIND_VIRTUALENV=ONLY \
+        -DACTS_USE_SYSTEM_NLOHMANN_JSON=ON \
+        -DACTS_FORCE_ASSERTIONS=ON \
+        -DACTS_ENABLE_LOG_FAILURE_THRESHOLD=ON \
+        -DACTS_BUILD_EXAMPLES_DD4HEP=ON \
+        -DACTS_BUILD_EXAMPLES_GEANT4=ON \
+        -DACTS_BUILD_EXAMPLES_PYTHIA8=ON \
+        -DACTS_BUILD_EXAMPLES_PYTHON_BINDINGS=ON \
+        -DACTS_BUILD_EXAMPLES_PODIO=ON \
+        -DACTS_BUILD_EXAMPLES_EDM4HEP=ON \
+        -DACTS_BUILD_EXAMPLES_HEPMC3=ON \
+        -DACTS_BUILD_PLUGIN_GEOMODEL=ON \
+        -DACTS_BUILD_FATRAS=ON \
+        -DACTS_BUILD_FATRAS_GEANT4=ON \
+        -DACTS_BUILD_ODD=ON \
+        -DACTS_BUILD_BENCHMARKS=ON \
+        -DACTS_BUILD_UNITTESTS=ON \
+        -DACTS_BUILD_INTEGRATIONTESTS=ON \
+        -DACTS_BUILD_EXAMPLES_UNITTESTS=ON
+else
+    echo "build directory already exists: $build"
+fi
---
+++ acts/setup-perf-spack.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+script=$(readlink -f "$0")
+script_dir=$(dirname "$script")
+
+. "$script_dir/setup-dev-spack.sh"
+
+cmake -S "$source" -B "$build" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_FLAGS_RELEASE="-O3 -DNDEBUG -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer" \
+  -DACTS_FORCE_ASSERTIONS=OFF
